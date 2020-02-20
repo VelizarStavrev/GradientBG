2878837d716bec6a483704996b42c6a687345756
VelizarStavrev <velizar.g.stavrev@gmail.com>
2020-02-20 19:12:03 +0200

    updates

--- script.js
+++ script.js
@@ -14,6 +14,52 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+window.addEventListener('load', setGradient);
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+function horizontal() {
+	body.style.background = 
+	"linear-gradient(to left, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function vertical() {
+	body.style.background = 
+	"linear-gradient(to top, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function diagonal() {
+	body.style.background = 
+	"linear-gradient(315deg, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function radial() {
+	body.style.background = 
+	"radial-gradient(" 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
